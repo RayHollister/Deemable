7d5070370e96041340e77cef458502a5af8ecc5f
Ray Hollister <github@rayhollister.com>
2023-12-07 13:53:25 -0500

    Got rid of unnecessary plugins

--- media/wiziapp_data_files/resources/handshake.js
+++
@@ -1,1 +0,0 @@
-var handshake = {"header":{"action":"handshake","status":true,"code":200,"message":""},"udid":"wordpress-cms","status":"active","status_message":"","features":{"notifications":true,"purchases":true},"need_update":{"css":false,"images":false,"config":false,"privacy":false},"alerts":[],"cms":{"access_point":"deemable.com\/?wiziapp","use_ssl":false}};
